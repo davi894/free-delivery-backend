334bec77ad6e2788beb477cec3251525c5e0b14d
davi894 <moisesdavi005@gmail.com>
2023-06-28 12:56:44 -0300

    fix: add fields address in entity

---
+++ prisma/migrations/20230628155527_add_address_fields/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - Added the required column `city` to the `Address` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `country` to the `Address` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `number` to the `Address` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `postalCode` to the `Address` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `state` to the `Address` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `street` to the `Address` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Address" ADD COLUMN     "city" TEXT NOT NULL,
+ADD COLUMN     "country" TEXT NOT NULL,
+ADD COLUMN     "latitude" DOUBLE PRECISION,
+ADD COLUMN     "longitude" DOUBLE PRECISION,
+ADD COLUMN     "number" TEXT NOT NULL,
+ADD COLUMN     "postalCode" TEXT NOT NULL,
+ADD COLUMN     "state" TEXT NOT NULL,
+ADD COLUMN     "street" TEXT NOT NULL;
